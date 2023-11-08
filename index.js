717753f0655f5127e224350cf10944e968d79661
Rifa9oct <kohinuraktherr@gmail.com>
2023-11-08 13:44:16 +0600

    set cookie and remove cookie

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
@@ -7,7 +8,12 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+app.use(cors({
+  origin:[
+    "http://localhost:5173"
+  ],
+  credentials:true
+}));
 app.use(express.json());
 
 
@@ -91,6 +97,23 @@ async function run() {
       res.send(result);
     })
 
+    //auth related api
+    app.post('/jwt', async(req,res)=>{
+      const user = req.body;
+      console.log("user for token ",user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn:"1h"})
+      res.cookie('token', token,{
+        httpOnly:true,
+        secure:true,
+        sameSite:'none'
+      })
+      res.send({success: true});
+    })
+
+    app.post('/logout', async(req,res)=>{
+      const user = req.body;
+      res.clearCookie('token', {maxAge: 0}).send({success:true})
+    })
 
 
 
