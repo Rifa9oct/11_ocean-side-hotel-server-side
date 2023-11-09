65483db091d103bcf6edd1340e6a61e7c81d552f
Rifa9oct <kohinuraktherr@gmail.com>
2023-11-09 16:51:43 +0600

    decriment roome availability

--- index.js
+++ index.js
@@ -10,30 +10,30 @@ const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors({
-  origin:[
+  origin: [
     "http://localhost:5173",
     "https://ocean-side-hotel.web.app",
     "https://ocean-side-hotel.firebaseapp.com"
   ],
-  credentials:true
+  credentials: true
 }));
 app.use(express.json());
 app.use(cookieParser());
 
-const logger = (req,res,next)=>{
-  console.log("log info: ",req.method, req.url);
+const logger = (req, res, next) => {
+  console.log("log info: ", req.method, req.url);
   next();
 }
 
-const verifyToken = (req, res , next) =>{
+const verifyToken = (req, res, next) => {
   const token = req?.cookies?.token;
   console.log("token in the middleware: ", token);
-  if(!token){
-    return res.status(401).send({message: "unauthorized access"});
+  if (!token) {
+    return res.status(401).send({ message: "unauthorized access" });
   }
-  jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err, decoded)=>{
-    if(err){
-      return res.status(401).send({message: "unauthorized access"});
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "unauthorized access" });
     }
     req.user = decoded;
     next();
@@ -62,7 +62,7 @@ async function run() {
     const bookingsCollection = client.db('hotelDB').collection('bookings');
     const reviewsCollection = client.db('hotelDB').collection('reviews');
 
-    app.get('/rooms',async (req, res) => {
+    app.get('/rooms', async (req, res) => {
       // console.log("cookies", req.cookies)
       const cursor = roomsCollection.find();
       const result = await cursor.toArray();
@@ -76,7 +76,7 @@ async function run() {
       res.send(result);
     })
 
-    app.get('/reviews',async (req, res) => {
+    app.get('/reviews', async (req, res) => {
       const cursor = reviewsCollection.find();
       const result = await cursor.toArray();
       res.send(result);
@@ -96,10 +96,10 @@ async function run() {
       res.send(result);
     })
 
-    app.get("/bookings", logger ,verifyToken, async (req, res) => {
-      console.log("token owner info",req.user);
-      if(req.user.email !== req.query.email){
-        return res.status(403).send({message: "forbidden access"});
+    app.get("/bookings", logger, verifyToken, async (req, res) => {
+      console.log("token owner info", req.user);
+      if (req.user.email !== req.query.email) {
+        return res.status(403).send({ message: "forbidden access" });
       }
       let query = {};
       if (req.query?.email) {
@@ -140,22 +140,38 @@ async function run() {
       res.send(result);
     })
 
-    //auth related api
-    app.post('/jwt', async(req,res)=>{
-      const user = req.body;
-      console.log("user for token ",user);
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn:"1h"})
-      res.cookie('token', token,{
-        httpOnly:true,
-        secure:true,
-        sameSite:'none'
-      })
-      res.send({success: true});
+    //decriment booking
+    app.patch("/rooms/:id", async (req, res) => {
+      const id = req.params.id;
+      const currentavailability = req.body;
+      console.log(currentavailability)
+      const filter = { _id: new ObjectId(id) }
+      const options = { upsert: true }
+      const updateAvailability = {
+        $set: {
+          availability: currentavailability.dec
+        }
+      }
+      const result = await roomsCollection.updateOne(filter, updateAvailability, options);
+      res.send(result);
     })
 
-    app.post('/logout', async(req,res)=>{
+    //auth related api
+    app.post('/jwt', async (req, res) => {
       const user = req.body;
-      res.clearCookie('token', {maxAge: 0}).send({success:true})
+      console.log("user for token ", user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1h" })
+      res.cookie('token', token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none'
+      })
+      res.send({ success: true });
+    })
+
+    app.post('/logout', async (req, res) => {
+      const user = req.body;
+      res.clearCookie('token', { maxAge: 0 }).send({ success: true })
     })
 
     // Send a ping to confirm a successful connection
